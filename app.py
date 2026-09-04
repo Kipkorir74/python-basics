b3f1b0ad2db4c1e7e98a4457294ddabf0e31270f
Kipkorir74 <grahamhim74@gmail.com>
2026-09-04 15:17:19 +0300

    input output

---
+++ app.py
@@ -0,0 +1,30 @@
+# print("-------------------")
+
+# print("LEARN \"PYTHON\"")
+
+# print("Path: C:\\User\\Documents\\Python\\app.py")
+
+# print("Nipe bega \t Mabega")
+# print("Nipe bega \t")
+
+# print("Nipe bega \nMabega")
+
+# print("Nipe bega \tMabega")
+
+# name="Maria"
+# language="Python"
+
+# print("My name is", name)
+# print(name, "is learning", language)
+# print(name, "wants to become a genius in", language)
+
+# domain = "datawithbaraa.com"
+
+# print("info@"+domain)
+# print("support@"+domain)
+# print("www."+domain)
+
+
+name=input("Hello, please enter your name: ")
+age=28
+print(name, "is", age, "years old")
