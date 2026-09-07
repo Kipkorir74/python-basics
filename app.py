e7107a58f925923d6d757728dc32d3185751722e
Kipkorir74 <grahamhim74@gmail.com>
2026-09-07 11:27:48 +0300

    String basics

--- app.py
+++ app.py
@@ -25,6 +25,56 @@
 # print("www."+domain)
 
 
-name=input("Hello, please enter your name: ")
-age=28
-print(name, "is", age, "years old")
+# name=input("Hello, please enter your name: ")
+# age=28
+# print(name, "is", age, "years old")
+
+# Data Types
+# a = 10 # integer number
+# b = 3.14 # float number
+# c = "Hello" # string
+# d = True # boolean
+# e =False # boolean
+# f = None # NoneType
+# g ="" # empty string
+
+# text = "Hello"
+# number = 11
+
+# # print(text)
+# print(number)
+# print(len(text))
+# print(type(number))
+
+# print(text.upper())
+# print(number.bit_length())
+
+# # Types
+# name="Kip"
+# type_of_name=type(name)
+# print(type_of_name)
+
+# age=28
+# type_of_age=type(age)
+# print("Your Age is:" + str(age))
+
+#Math
+# password ="12ed8"
+# print("The length of the password is:", len(password))
+
+# if len (password) < 8:
+#     print("Password is too short")
+
+# text = """
+#     Python is a high-level, interpreted programming language that 
+#     is widely used for web development, data analysis, artificial 
+#     intelligence, scientific computing, and more. 
+#     It was created by Guido van Rossum and first released in 1991. 
+#     Python emphasizes code readability and simplicity, 
+#     making it an excellent choice for beginners and experienced developers alike.
+# """
+# print(text.count("and"))
+
+# Transformations
+price = "123,7889"
+print(price.replace(",", "")) 
