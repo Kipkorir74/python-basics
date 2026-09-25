6cb7c4b581cb674eab06e5519087940140d4a065
Kipkorir74 <grahamhim74@gmail.com>
2026-09-25 10:52:57 +0300

    magicmethods

---
+++ OOP/magicmethods.py
@@ -0,0 +1,74 @@
+import uuid
+class Archer:
+
+    def __init__(self,hp, mana, arrows): 
+        # Initialize the properties
+        self.hp = hp
+        self.mana = mana
+        self.arrows = arrows
+        self._id = uuid.uuid4()
+    def __str__(self):
+        return f"Archer with {self.hp} hp and {self.mana} mana and {self.arrows} arrows"
+
+    def __repr__(self):
+        return f"Archer ({self.hp}, {self.mana}, {self.arrows})" 
+
+    def __add__(self, other):
+        if not isinstance(other,Archer):
+            return NotImplemented
+        new_hp = self.hp + other.hp
+        new_mana = self.mana + other.mana
+        new_arrows = self.arrows + other.arrows
+        return Archer(new_hp, new_mana, new_arrows)
+    def __eq__(self, other):
+        if not isinstance(other,Archer):
+            return False
+        return self.hp == other.hp and self.mana == other.mana and self.arrows  == other.arrows
+    def __gt__(self, other):  #greater than
+         if not isinstance(other,Archer):
+             return NotImplemented
+         return self.hp > other.hp 
+    def __hash__(self):
+        return hash(self._id)        
+    
+archer1 = Archer(200,100,3)
+archer2 = Archer(100,100,3)
+archer3 = archer1 + archer2
+# print(hash(archer1))
+# archer1.mana = 100
+# print(hash(archer1))
+# print(archer3)
+# print(archer1 == archer2)
+# print (archer1 > archer2)
+
+new_dict = {archer1:"test"}
+
+class Company:
+    def __init__(self,size):
+        self.size = size
+        self.archers = []
+        self.index = 0
+    def add_archer(self,archer):
+        if not isinstance(archer,Archer):
+            raise TypeError("Only Archers Allowed")
+        if len(self.archers ) > self.size:
+            raise ValueError("Company Already Full")
+        self.archers.append(archer)
+    def __add__(self, other):
+        if not isinstance(other,Archer):
+            raise TypeError("Only adding Archers Allowed")
+        self.add_archer(other)
+        return self
+
+    def __iter__(self):
+        return iter(self.archers)
+
+
+archer2 = Archer(100,100,5)
+company = Company(5)
+new_company = company + archer2+ archer2
+
+# print(company.archers)
+# print(new_company.archers)
+for archer in new_company.archers:
+    print(archer)
